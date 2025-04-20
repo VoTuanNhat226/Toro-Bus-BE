8df49ce36c49d81de4b28b2e11663153c4b9e937
NhatVT2 <NhatVT2@fpt.com>
2025-04-20 23:57:39 +0700

    init project

---
+++ src/main/java/com/vtn/toro/ToroApplication.java
@@ -0,0 +1,13 @@
+package com.vtn.toro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToroApplication.class, args);
+	}
+
+}
